edf710fbc658dd85183f55cc339e724ce693839c
sajjad <mynameissajjad@gmail.com>
2020-09-16 07:49:49 +0430

    some methods working with sqlite

---
+++ v4/test.php
@@ -0,0 +1,12 @@
+<?php
+
+$db = new SQLite3('test.db');
+
+$sql = "SELECT name FROM cars WHERE name = 'Audi'";
+
+$escaped = SQLite3::escapeString($sql);
+
+var_dump($sql);
+var_dump($escaped);
+
+?>
